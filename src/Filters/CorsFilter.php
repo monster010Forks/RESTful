90f1ed033105f3f699ab2dedd0a019219cce6751
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2019-09-30 21:44:35 -0700

    Update CorsFilter.php

--- src/Filters/CorsFilter.php
+++ src/Filters/CorsFilter.php
@@ -25,18 +25,18 @@
         public function before(RequestInterface $request)
         {
             // Domains within the config
-            $domains   = $this->config->corsDomains;
+            $domains  = $this->config->corsDomains;
             // Shared response service
-            $response  = Services::response();
+            $response = Services::response();
 
             // Configuring the 'Access-Control-Allow-Origin' header
             if ($this->config->enableCors === false) {
+
                 // Set the Access-Control-Allow-Origin header to all (*)
                 $response->setHeader('Access-Control-Allow-Origin', '*');
                 $allow_all = true;
-
             } else {
-                // Localhost
+                // Localhost array
                 $localhosts = [
                     'http://localhost',
                     '127.0.0.1'
@@ -46,7 +46,7 @@
 
                 // Get a string of merged array keys for $response->setHeader()
                 $these_domains = implode(', ', $merged_config);
-                
+
                 // Set the Access-Control-Allow-Origin header to $these_domains
                 $response->setHeader('Access-Control-Allow-Origin', $these_domains);
                 $allow_all = false;
@@ -54,12 +54,18 @@
 
             // Handle the allowance of hosts when cors is enabled
             if ($allow_all === false) {
-                // TODO - Allow only hosts within CORS list in config
+                // Get the 'Access-Control-Allow-Origin' value as a string
                 $domains = (string)$response->getHeader('Access-Control-Allow-Origin')->getValue();
+
+                // Get the $_SERVER['HTTP_HOST']
                 $http_host = 'http://' . $request->getServer('HTTP_HOST');
+
+                // If the $_SERVER['HTTP_HOST'] is found within the config, return true and false otherwise
                 $is_allowed = strpos($domains, $http_host) !== false;
 
+                // If we are not allowed
                 if ( ! $is_allowed) {
+                    // Send the response and exit()
                     $response
                         ->setStatusCode(HTTP_UNAUTHORIZED)
                         ->setJSON([
