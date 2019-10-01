c96f2f54fa594ef622b3579a4c0c79a2eadc9e52
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2019-09-30 20:48:45 -0700

    Update CorsFilter.php
    Basic CORS implementation (open to ideas and refactors too =D)

--- src/Filters/CorsFilter.php
+++ src/Filters/CorsFilter.php
@@ -4,6 +4,7 @@
     use CodeIgniter\Filters\FilterInterface;
     use CodeIgniter\HTTP\ResponseInterface;
     use CodeIgniter\HTTP\RequestInterface;
+    use Config\Services;
 
     /**
      * Class CorsFilter
@@ -23,7 +24,52 @@
          */
         public function before(RequestInterface $request)
         {
+            // Domains within the config
+            $domains   = $this->config->corsDomains;
+            // Shared response service
+            $response  = Services::response();
 
+            // Configuring the 'Access-Control-Allow-Origin' header
+            if ($this->config->enableCors === false) {
+                // Set the Access-Control-Allow-Origin header to all (*)
+                $response->setHeader('Access-Control-Allow-Origin', '*');
+                $allow_all = true;
+
+            } else {
+                // Localhost
+                $localhosts = [
+                    'http://localhost',
+                    '127.0.0.1'
+                ];
+                // Merge the config arrays
+                $merged_config = array_merge($domains, $localhosts);
+
+                // Get a string of merged array keys for $response->setHeader()
+                $these_domains = implode(', ', $merged_config);
+                
+                // Set the Access-Control-Allow-Origin header to $these_domains
+                $response->setHeader('Access-Control-Allow-Origin', $these_domains);
+                $allow_all = false;
+            }
+
+            // Handle the allowance of hosts when cors is enabled
+            if ($allow_all === false) {
+                // TODO - Allow only hosts within CORS list in config
+                $domains = (string)$response->getHeader('Access-Control-Allow-Origin')->getValue();
+                $http_host = 'http://' . $request->getServer('HTTP_HOST');
+                $is_allowed = strpos($domains, $http_host) !== false;
+
+                if ( ! $is_allowed) {
+                    $response
+                        ->setStatusCode(HTTP_UNAUTHORIZED)
+                        ->setJSON([
+                            'message' => 'Requests from this host are not allowed',
+                            'status'  => HTTP_UNAUTHORIZED
+                        ])
+                        ->send();
+                        exit();
+                }
+            }
         }
 
         //--------------------------------------------------------------------
