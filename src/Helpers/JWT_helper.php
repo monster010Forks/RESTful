71f001817c87712a5eb3d59005cc0f16f14ff890
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2019-10-02 11:38:27 -0700

    Update JWT_helper.php

--- src/Helpers/JWT_helper.php
+++ src/Helpers/JWT_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This is a helper file the JWT library
+ * This is a helper file for the built-in JWT library
  *
  * @package  RESTful\Helpers
  * @author   Jason Napolitano <jnapolitanoit@gmail.com>
