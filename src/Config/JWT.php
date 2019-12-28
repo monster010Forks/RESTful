36e3488337e70c9decabe5c395b280ea600e8ee3
JNapolitanoIT <jnapolitanoit@gmail.com>
2019-12-27 22:02:41 -0800

    Update JWT Config
    Changed the documentation on a class property to be a little more verbose and descriptive

--- src/Config/JWT.php
+++ src/Config/JWT.php
@@ -1,21 +1,29 @@
 <?php namespace RESTful\Config;
 
 /**
- * JWT Library configuration class
+ * JWT Library configuration class. These config values are
+ * used library-wide.
  *
  * @package RESTful\Config
  */
 class JWT
 {
     /**
-     * Library-wide secret key
+     * Library-wide secret key. This is used for encoding
+     * all JWT's (unless explicitly different) and will be
+     * required to decode the JWT as well
      *
      * @var string $secretKey
      */
     public $secretKey = 'super-secret-key';
 
     /**
-     * Library-wide hashing algorithm
+     * Library-wide hashing algorithm. This is the default
+     * hashing algorithm. There is no real need to change this
+     * value unless absolutely required.
+     *
+     * OPTIONS:
+     *  -
      *
      * @var string $algorithm
      */
