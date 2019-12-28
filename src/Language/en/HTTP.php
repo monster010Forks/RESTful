c3434710a31c302944182b9b2c972f824a22499a
JNapolitanoIT <jnapolitanoit@gmail.com>
2019-12-27 22:47:32 -0800

    Update HTTP.php
    Updated the language strings in HTTP.php

--- src/Language/en/HTTP.php
+++ src/Language/en/HTTP.php
@@ -7,7 +7,7 @@
  * autoloaded in app/Config/Autoload.php
  *
  * USAGE:
- *  - lang('HTTP.200') // PRODUCES: The resource requested has responded successfully
+ *  - lang('HTTP.200') // PRODUCES: The resource has responded successfully
  *  - lang('HTTP.404') // PRODUCES: The resource requested does not exist
  *
  * @package      RESTful
@@ -15,17 +15,17 @@
  * @link         https://github.com/jason-napolitano/restful
  */
 return [
-    '102' => 'The resource request is currently processing',
-    '200' => 'The resource requested has responded successfully',
-    '201' => 'The resource requested was created successfully',
-    '204' => 'The resource requested was successful with no content returned',
-    '301' => 'The resource requested has been permanently moved',
-    '400' => 'The request made to this resource was not correct',
+    '102' => 'This request is currently processing',
+    '200' => 'This request has responded successfully',
+    '201' => 'This resource was created successfully',
+    '204' => 'This request was successful with no content returned',
+    '301' => 'This resource has been permanently moved',
+    '400' => 'This request made to this resource was not correct',
     '401' => 'You are unauthorized to view this resource',
     '402' => 'Payment is required before you may proceed',
-    '403' => 'Access to this resource is explicitly forbidden',
-    '404' => 'The resource requested does not exist',
-    '409' => 'The resource requested could not continue due to a conflict',
+    '403' => 'Access to this resource is forbidden',
+    '404' => 'The requested resource does not exist',
+    '409' => 'The request could not continue due to a conflict',
     '500' => 'The server has experienced an unexpected error',
-    '503' => 'The resource requested is currently unavailable',
+    '503' => 'The requested resource is currently unavailable',
 ];
