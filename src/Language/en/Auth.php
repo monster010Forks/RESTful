214a31e0fa25e3463c51eb105d77dbcdec4fe85b
JNapolitanoIT <jnapolitanoit@gmail.com>
2019-12-29 23:54:24 -0800

    Added auth language files. Getting ready for authentication

---
+++ src/Language/en/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Language strings for library authentication.
+ *
+ * Use anywhere in the application as long as RESTful is PSR4
+ * autoloaded in app/Config/Autoload.php
+ *
+ * USAGE:
+ *  - lang('Auth.userAlreadyExists') // PRODUCES: This user account already exists
+ *
+ * @package      RESTful
+ * @author       Jason Napolitano
+ * @link         https://github.com/jason-napolitano/restful
+ */
+return [
+    'userAlreadyExists' => 'This user account already exists'
+];
---
+++ src/Language/es/Auth.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Cadenas de idiomas para la autenticación de la biblioteca.
+ *
+ *
+ * Úselo en cualquier lugar de la aplicación siempre que RESTful sea PSR4
+ * cargado automáticamente en la app/Config/Autoload.php
+ *
+ * USAR:
+ *  - lang('Auth.userAlreadyExists') // PRODUCES: Esta cuenta de usuario ya existe
+ *
+ * @package      RESTful
+ * @author       Jason Napolitano
+ * @link         https://github.com/jason-napolitano/restful
+ */
+return [
+    'userAlreadyExists' => 'Esta cuenta de usuario ya existe'
+];
