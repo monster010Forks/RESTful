0013abb127745b47b7500b718d86cb1efcb6ba95
jason-napolitano <jnapolitanoit@gmail.com>
2020-01-09 23:44:47 -0800

    Update Auth Service

---
+++ src/Services/Auth/AuthFactory.php
@@ -0,0 +1,17 @@
+<?php namespace RESTful\Services\Auth {
+
+    /**
+     * AuthFactory Class
+     *
+     * @package RESTful\Services\Auth
+     */
+    class AuthFactory
+    {
+        /**
+         * Config object
+         *
+         * @var mixed $config
+         */
+        public $config;
+    }
+}
---
+++ src/Services/Auth/Handlers/JWTHandler.php
@@ -0,0 +1,70 @@
+<?php namespace RESTful\Services\Auth\Handlers{
+
+    use RESTful\Services\Auth\AuthInterface;
+
+    /**
+     * The main library class for 'jwt' authentication
+     *
+     * @package RESTful\Services\Auth\Handlers
+     */
+    class JWTHandler implements AuthInterface
+    {
+        /**
+         * Authenticate a user
+         *
+         * @param string $username
+         * @param string $password
+         *
+         * @return void
+         */
+        public function authenticate(string $username = null, string $password = null): void
+        {
+            // TODO: Implement authenticate() method.
+        }
+
+        /**
+         * Destroy a current session
+         *
+         * @return void
+         */
+        public function destroy(): void
+        {
+            // TODO: Implement destroy() method.
+        }/**
+     * Was a user successfully authenticated?
+     *
+     * @return bool
+     * @var string $username
+     *
+     */
+        public function isAuthenticated(string $username = null): bool
+        {
+            return false;
+        }
+
+        /**
+         * Check if a user exists
+         *
+         * @param string $username
+         *
+         * @return bool
+         */
+        public function alreadyExists(string $username): bool
+        {
+            // TODO: Implement alreadyExists() method.
+        }
+
+        /**
+         * Verify a username/password
+         *
+         * @param string $username
+         * @param string $password
+         *
+         * @return bool
+         */
+        public function verifyCredentials(string $username, string $password): bool
+        {
+            // TODO: Implement verifyCredentials() method.
+        }
+    }
+}
