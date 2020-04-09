d751890d31a57306605c3b3939cecd5cebe5f707
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2020-04-09 11:38:21 -0700

    Update 2019-09-21-033709_CreateSessionsTable.php

--- src/Database/Migrations/2019-09-21-033709_CreateSessionsTable.php
+++ src/Database/Migrations/2019-09-21-033709_CreateSessionsTable.php
@@ -10,29 +10,29 @@ class CreateSessionsTable extends Migration
         $this->table = $this->config->tables['sessions'] ?? 'api_sessions';
 
         $this->forge->addField([
-                                   'id'         => [
-                                       'type'       => 'VARCHAR',
-                                       'constraint' => 128,
-                                       'null'       => false,
-                                   ],
-                                   'ip_address' => [
-                                       'type'       => 'VARCHAR',
-                                       'constraint' => 45,
-                                       'null'       => false,
-                                   ],
-                                   'timestamp'  => [
-                                       'type'       => 'INT',
-                                       'constraint' => 10,
-                                       'unsigned'   => true,
-                                       'null'       => false,
-                                       'default'    => 0,
-                                   ],
-                                   'data'       => [
-                                       'type'    => 'TEXT',
-                                       'null'    => false,
-                                       'default' => '',
-                                   ],
-                               ]);
+            'id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 128,
+                'null'       => false,
+            ],
+            'ip_address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 45,
+                'null'       => false,
+            ],
+            'timestamp' => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+                'null'       => false,
+                'default'    => 0,
+            ],
+            'data' => [
+                'type'    => 'TEXT',
+                'null'    => false,
+                'default' => '',
+            ],
+        ]);
         $this->forge->addKey('id', true);
         $this->forge->addKey('timestamp');
         $this->forge->createTable($this->table, true);
